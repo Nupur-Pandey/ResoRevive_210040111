a8b75b78d22345fd0ed682431eced7c5c11ee3a5
Nupur-Pandey <96698480+Nupur-Pandey@users.noreply.github.com>
2024-07-13 00:00:44 +0530

    Add files via upload

---
+++ didi_didi.py
@@ -0,0 +1,178 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader, Dataset
+from torchvision import models, transforms
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+class CornMaizeDataset(Dataset):
+    def __init__(self, file_paths, labels, transform=None):
+        self.file_paths = file_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.file_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
+
+# Helper function to get the file paths and labels
+def get_file_paths_and_labels(root_dir):
+    file_paths = []
+    labels = []
+    classes = os.listdir(root_dir)
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+    
+    for class_name in classes:
+        class_dir = os.path.join(root_dir, class_name)
+        for img_name in os.listdir(class_dir):
+            file_paths.append(os.path.join(class_dir, img_name))
+            labels.append(class_to_idx[class_name])
+    
+    return file_paths, labels
+
+# Define root directory for your dataset
+##########################################################################
+root_dir = "corn_maize_dataset"
+##########################################################################
+###################################################################################
+
+file_paths, labels = get_file_paths_and_labels(root_dir)
+
+
+train_files, test_files, train_labels, test_labels = train_test_split(file_paths, labels, test_size=0.2, random_state=42)
+train_files, val_files, train_labels, val_labels = train_test_split(train_files, train_labels, test_size=0.25, random_state=42)  # 0.25 x 0.8 = 0.2
+
+
+model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+
+
+for param in model.parameters():
+    param.requires_grad = False
+
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 4)  
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+
+data_transforms = models.ResNet50_Weights.DEFAULT.transforms()
+
+train_dataset = CornMaizeDataset(train_files, train_labels, transform=data_transforms)
+val_dataset = CornMaizeDataset(val_files, val_labels, transform=data_transforms)
+test_dataset = CornMaizeDataset(test_files, test_labels, transform=data_transforms)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
+
+
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.Adam(model.fc.parameters(), lr=0.001, weight_decay=1e-4)
+
+scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+
+import time
+from copy import deepcopy
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+                dataloader = train_loader
+            else:
+                model.eval()   # Set model to evaluate mode
+                dataloader = val_loader
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                # Forward
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # Backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # Statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / len(dataloader.dataset)
+            epoch_acc = running_corrects.double() / len(dataloader.dataset)
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:.4f}')
+
+    model.load_state_dict(best_model_wts)
+    return model
+
+# Train the model
+model = train_model(model, criterion, optimizer, scheduler, num_epochs=25)
+
+
+def evaluate_model(model, dataloader):
+    model.eval()  # Set model to evaluate mode
+    running_corrects = 0
+
+    for inputs, labels in dataloader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        running_corrects += torch.sum(preds == labels.data)
+
+    accuracy = running_corrects.double() / len(dataloader.dataset)
+    print(f'Test Accuracy: {accuracy:.4f}')
+
+evaluate_model(model, test_loader)
